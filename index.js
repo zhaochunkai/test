3dd2899989034ce9d1ffe248812462cf4595c4c1
zhaochunkai <1559941352@qq.com>
2020-03-07 15:53:26 +0800

    新建了一个js文件

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: zhaochunkai
+* @Date:   2020-03-07 15:50:13
+* @Last Modified by:   zhaochunkai
+* @Last Modified time: 2020-03-07 15:51:13
+*/
+
+function test () {
+	console.log(1234)
+}
+
+test()
